f330a841d15e505fe0d4adc81f669a442ac963c7
Pluto <nickjareddelgado@gmail.com>
2022-07-17 18:46:03 -0600

    Started Laravel Project

---
+++ LaravelApp/resources/views/components/listing-card.blade.php
@@ -0,0 +1,21 @@
+@props(['listItem'])
+
+<x-card>
+    <div class="flex">
+        <img
+            class="hidden w-48 mr-6 md:block"
+            src="{{asset('images/no-image.png')}}"
+            alt=""
+        />
+        <div>
+            <h3 class="text-2xl">
+                <a href="/listings/{{$listItem->id}}">{{$listItem->title}}</a>
+            </h3>
+            <div class="text-xl font-bold mb-4">{{$listItem->company}}</div>
+            <x-listing-tags :tagsCsv="$listItem->tags" />
+            <div class="text-lg mt-4">
+                <i class="fa-solid fa-location-dot"></i> {{$listItem->location}}
+            </div>
+        </div>
+    </div>
+</x-card>
